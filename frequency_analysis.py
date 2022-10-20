819f524a8914f41a5dfdae7f3d06892b24b6f421
mvangent <melvin.vangent@gmail.com>
2022-10-20 12:15:22 +0200

    Fix substitution_cipher decrypt, create frequency analysis class

---
+++ frequency_analysis.py
@@ -0,0 +1,44 @@
+cipher = """lrvmnir bpr sumvbwvr jx bpr lmiwv yjeryrkbi jx qmbm wi
+bpr xjvni mkd ymibrut jx irhx wi bpr riirkvr jx
+ymbinlmtmipw utn qmumbr dj w ipmhh but bj rhnvwdmbr bpr
+yjeryrkbi jx bpr qmbm mvvjudwko bj yt wkbrusurbmbwjk
+lmird jk xjubt trmui jx ibndt
+  wb wi kjb mk rmit bmiq bj rashmwk rmvp yjeryrkb mkd wbi
+iwokwxwvmkvr mkd ijyr ynib urymwk nkrashmwkrd bj ower m
+vjyshrbr rashmkmbwjk jkr cjnhd pmer bj lr fnmhwxwrd mkd
+wkiswurd bj invp mk rabrkb bpmb pr vjnhd urmvp bpr ibmbr
+jx rkhwopbrkrd ywkd vmsmlhr jx urvjokwgwko ijnkdhrii
+ijnkd mkd ipmsrhrii ipmsr w dj kjb drry ytirhx bpr xwkmh
+mnbpjuwbt lnb yt rasruwrkvr cwbp qmbm pmi hrxb kj djnlb
+bpmb bpr xjhhjcwko wi bpr sujsru msshwvmbwjk mkd
+wkbrusurbmbwjk w jxxru yt bprjuwri wk bpr pjsr bpmb bpr
+riirkvr jx jqwkmcmk qmumbr cwhh urymwk wkbmvb"""
+
+
+class Attack:
+    def __init__(self):
+        self.alphabet = "abcdefghijklmnopqrstuvwxyz"
+        self.freq = {}
+        
+    def calculate_freq(self, cipher):
+        letter_count = 0
+        for c in self.alphabet:
+            self.freq[c] = 0
+
+        for c in cipher:
+            if c in self.freq:
+                self.freq[c] += 1
+                letter_count += 1
+
+        for key in self.freq:
+            self.freq[key] = round(self.freq[key] / letter_count, 4)
+
+    def print_freq(self):
+        for key in self.freq:
+            print(key + " : " + str(self.freq[key]))
+
+
+attack = Attack()
+
+attack.calculate_freq(cipher)
+attack.print_freq()
--- substitution_cipher.py
+++ substitution_cipher.py
@@ -21,8 +21,31 @@ def encrypt(key, message):
             cipher += c
     return cipher
 
+def get_decryption_key(key):
+    dkey = {}
+
+    for c in key:
+        dkey[key[c]] = c 
+
+    return dkey
+
+def decrypt(key, cipher):
+    decryption_key = get_decryption_key(key)
+    
+    decoded_message = ''
+    for c in cipher:
+        if c in key:
+            decoded_message += decryption_key[c]
+        else:
+            decoded_message += c
+    
+    return decoded_message
 
 key = generate_key()
 message = "YOU ARE AWESOME"
 cipher = encrypt(key, message)
 print(cipher)
+
+print(decrypt(key, cipher))
+
+
