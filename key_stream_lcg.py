98dddcd05007faad4f1f77bf5d59d3fb55e4aa1a
mvangent <melvin.vangent@gmail.com>
2022-11-02 17:36:18 +0100

    Add bit flip to stream cipher

--- key_stream_lcg.py
+++ key_stream_lcg.py
@@ -20,12 +20,27 @@ class KeyStream:
 def encrypt(key, message):
     return bytes([message[i] ^ key.get_key_byte() for i in range(len(message))])
 
+# In the transmit function flip the 6th least significant bit of the 4th byte.
+def transmit(cipher):
+    b = []
+    
+    for i in range(len(cipher)):
+        c = cipher[i]
+        if i == 3:
+            c = c ^ 2**5
+        
+        b.append(c)
+    
+    return bytes(b)
 
 # This is Alice
 key = KeyStream(10)
 message = "Hello, World!"
 message = message.encode()
 cipher = encrypt(key, message)
+
+# This is the poor transmission
+cipher = transmit(cipher)
 
 # This is Bob
 key = KeyStream(10)
